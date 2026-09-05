74d3d85187321f101bb3a97f8bf7b1d80b5d3ca3
Karan Patel <55875282+karanpokar@users.noreply.github.com>
2026-09-05 09:58:19 +0530

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        let Obj={}
+            for(var i=0;i<nums.length;i++){
+                let count=Obj[`${nums[i]}`] || 0
+                Obj[`${nums[i]}`]= count + 1
+            }
+            let keys=Object.keys(Obj)
+            keys.sort((a, b) => Obj[b] - Obj[a])
+            return keys.slice(0, k)
+        }
+    
+}
