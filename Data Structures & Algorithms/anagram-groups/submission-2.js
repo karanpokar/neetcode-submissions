f8beda13ed26194e5a20174f951a427507a36c99
Karan Patel <55875282+karanpokar@users.noreply.github.com>
2026-09-05 09:46:07 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let stringObj={}
+        if(strs==""){
+            return [[""]]
+        }
+        for(var i=0;i<strs.length;i++){
+            let string=strs[i]
+            let sorted=strs[i].split('').sort().join('')
+            let arr=stringObj[`${sorted}`] || []
+            let arrPush=[...arr,string]
+            stringObj[`${sorted}`]=arrPush
+        }
+        return Object.values(stringObj)
+    }
+}
