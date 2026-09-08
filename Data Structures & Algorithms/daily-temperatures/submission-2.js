7c415fa7635347fb7aaa8694893476439297e318
Karan Patel <55875282+karanpokar@users.noreply.github.com>
2026-09-08 08:18:26 +0530

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+       let result = new Array(temperatures.length).fill(0)
+        let stack = []
+        
+        const checkTop=(stack)=>{
+            return stack[stack.length-1]
+        }
+        for(var i=0;i<temperatures.length;i++){
+            // if(i==0){
+            //     stack.push(i)
+            // }
+            while(temperatures[checkTop(stack)]<temperatures[i] && stack.length>0){
+                result[checkTop(stack)]=i-checkTop(stack);
+                stack.pop()
+            }
+            stack.push(i)
+        }
+        //console.log(result)
+         return result
+    }
+   
+}
